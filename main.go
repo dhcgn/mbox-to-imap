5865281f38ede791f4af67c779889dd064c37c09
Daniel <dhcgn@users.noreply.github.com>
2025-10-25 13:16:36 +0200

    add config

--- main.go
+++ main.go
@@ -4,18 +4,37 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/spf13/cobra"
+
 	"github.com/dhcgn/mbox-to-imap/config"
 )
 
-// flags
-var ()
-
 func main() {
+	rootCmd := &cobra.Command{
+		Use:   "mbox-to-imap",
+		Short: "Import messages from mbox archives into an IMAP mailbox",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg, err := config.LoadConfig(cmd)
+			if err != nil {
+				return err
+			}
 
-	_, err := config.LoadConfig(os.Args[1:])
-	if err != nil {
-		fmt.Errorf("failed to load config: %w", err)
-		os.Exit(-1)
+			return run(cfg)
+		},
 	}
 
+	if err := config.RegisterFlags(rootCmd); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to register CLI flags: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(cfg config.Config) error {
+	// TODO: wire the configuration into the importer workflow.
+	return nil
 }
